47ed8bc91ac8b4a68e24fd203b3d892f2cf03d6a
Weverton dos Reis <wevertondosreis@gmail.com>
2022-12-08 18:05:47 -0300

    Listando os filtros

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from "react-native";
-import styled, {css} from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY'
 
@@ -7,22 +7,22 @@ type Props = {
     type: ButtonTypeStyleProps;
 }
 
-export const Container = styled(TouchableOpacity)<Props>`
+export const Container = styled(TouchableOpacity) <Props>`
     flex: 1;
     min-height: 56px;
     max-height: 56px;
-    background-color: ${({ theme, type}) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.GRAY_700};
+    background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.GRAY_700};
     border-radius: 6px;
     justify-content: center;
     align-items: center;
 `;
 
 export const Title = styled.Text`
-    ${({ theme }) => css `
-        font-size: ${theme.FONT_SIZE.MD}px;
-        color: ${theme.COLORS.WHITE};
-        font-family: ${theme.FONT_FAMILY.BOLD};
+    ${({ theme }) => css`
+      font-size: ${theme.FONT_SIZE.MD}px;
+       color: ${theme.COLORS.WHITE};
+       font-family: ${theme.FONT_FAMILY.BOLD};
     `};
 
-   
+ 
 `;
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState([]);
+
+
     return (
         <Container>
             <Header showBackButtom />
@@ -24,11 +31,32 @@ export function Players() {
 
 
             </Form>
-            
-            <Filter 
-                title="Time A"
-                isActive
-            />
+
+            <HeaderList>
+
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                    
+                </NumberOfPlayers>
+
+            </HeaderList>
+
+
+
 
 
         </Container>

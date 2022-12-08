5291a78e623d48ac287dd5db7b441deeb78b1913
Weverton dos Reis <wevertondosreis@gmail.com>
2022-12-08 14:33:37 -0300

    CSS Helper

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled , {css}  from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 
 import { UsersThree} from "phosphor-react-native"
@@ -16,6 +16,10 @@ export const Container = styled(TouchableOpacity)`
 
 
 export const Title = styled.Text`
+    
+
+
+
     font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
     font-family:${({theme}) => theme.FONT_FAMILY.REGULAR};
     color: ${({theme}) => theme.COLORS.GRAY_200};

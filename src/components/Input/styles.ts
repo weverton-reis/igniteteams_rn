f281dce965968a70c9084a928f86bf24c7b0d659
Weverton dos Reis <wevertondosreis@gmail.com>
2022-12-08 10:01:39 -0300

    Criado o componente Input

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,16 @@
+import { TextInput } from "react-native";
+
+import styled from "styled-components/native";
+
+
+
+export const Container = styled(TextInput) `
+    min-height: 56px;
+    max-height: 56px;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+    color:  ${({ theme }) => theme.COLORS.WHITE};
+    border-radius: 6px;
+    padding: 16px;
+    font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
+    font-family:${({theme}) => theme.FONT_FAMILY.REGULAR};
+`;
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,6 +4,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup(){
     return(
@@ -18,8 +19,11 @@ export function NewGroup(){
                     subTitle="crie a turma para adicionar as pessoas"
                 />
 
+                <Input/>
+
                 <Button
                     title="Criar"
+                    style={{marginTop: 20}}
 
                 />
             </Content>
